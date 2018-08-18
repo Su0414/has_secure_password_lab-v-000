69c98603dc4089f1b88bc3949b6475a674d71b09
Suchitra Naik <susanaik@gmail.com>
2018-08-18 13:05:56 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     if params[:user][:name] && params[:user][:name] != ''
-      user = User.find_by(name: params[:name])
+      user = User.find_by(name: params[:user][:name])
       binding.pry
       if user.authenticate(params[:user][:password])
         session[:user_id] = user.id
